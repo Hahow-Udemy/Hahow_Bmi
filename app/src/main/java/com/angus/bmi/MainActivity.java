30d3d5777cf6425c8af02eaaa54cfde6a468c2c6
pighaddt <pighaddt@gmail.com>
2019-07-11 16:46:20 +0800

    US & TW version

--- app/src/main/java/com/angus/bmi/MainActivity.java
+++ app/src/main/java/com/angus/bmi/MainActivity.java
@@ -1,14 +1,69 @@
 package com.angus.bmi;
 
+        import android.content.DialogInterface;
+        import android.support.v7.app.AlertDialog;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private TextView result;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        String hello = getString(R.string.hello);
+    }
 
+    private void findViews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        result = findViewById(R.id.result);
+        help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.help)
+                        .setMessage(R.string.bmi_info)
+                        .setPositiveButton(R.string.ok, null)
+                        .show();
+            }
+        });
+    }
+
+    public void bmi(View view) {
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity", "BMI:" + bmi);
+        Toast.makeText(this, getString(R.string.your_bmi_is) + bmi, Toast.LENGTH_LONG).show();
+        result.setText(getString(R.string.your_bmi_is) + bmi);
+        new AlertDialog.Builder(this)
+                .setTitle("BMI")
+                .setMessage(getString(R.string.your_bmi_is) + bmi)
+                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edWeight.setText("");
+                        edHeight.setText("");
+                    }
+                })
+                .show();
     }
 }
